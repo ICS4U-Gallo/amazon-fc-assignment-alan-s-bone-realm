acb61e885876143f7131bd8c591df2fb9276ab53
josh <joshua.chuang21@ycdsbk12.ca>
2019-10-23 19:33:03 -0400

    arcade lol

--- amazon_fc.py
+++ amazon_fc.py
@@ -1,4 +1,24 @@
 from typing import List, Dict
+import arcade
+
+screen_width = 800
+screen_height = 600
+
+def on_draw(delta_time):
+    arcade.start_render()
+    pass
+
+
+def on_key_press(key, modifiers):
+    pass
+
+
+def on_key_release(key, modifiers):
+    pass
+
+
+def on_mouse_press(x, y, button, modifiers):
+    pass
 
 
 class Item:
@@ -231,3 +251,34 @@ truck1 = Truck(12, [123, 456])
 package1.send_to_truck("8101 Leslie Street")
 # print(truck1.orders)
 # print(truck1.orders[123].address)
+
+def setup():
+    arcade.open_window(screen_width, screen_height, "My Arcade Game")
+    arcade.set_background_color(arcade.color.BLACK)
+    arcade.schedule(on_update, 1/60)
+
+    # Override arcade window methods
+    window = arcade.get_window()
+    window.on_draw = on_draw
+    window.on_key_press = on_key_press
+    window.on_key_release = on_key_release
+    window.on_mouse_press = on_mouse_press
+
+
+def main():
+    arcade.open_window(screen_width, screen_height, "Amazon FC")
+    arcade.set_background_color(arcade.color.BLACK)
+
+    # Call on_draw every 60th of a second
+    arcade.schedule(on_draw, 1/60)
+    window = arcade.get_window
+    window.on_draw = on_draw
+    window.on_key_press = on_key_press
+    window.on_key_release = on_key_release
+    window.on_mouse_press = on_mouse_press
+    
+
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
