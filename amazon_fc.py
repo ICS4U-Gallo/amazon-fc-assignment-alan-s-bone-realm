1af17a5f631105a91ba443debd821b8e0ab2855f
Joseph <joseph.zhang21@ycdsbk12.ca>
2019-10-23 13:46:28 -0400

    update

--- amazon_fc.py
+++ amazon_fc.py
@@ -2,13 +2,16 @@ from typing import List, Dict
 
 
 class Item:
-    """
+    """Item class
+
     Attrs:
         product_name (str): The name of the product.
-        product_number (int): An integer identification of the product.
+        product_number (int): The product's identification number.
         barcode (int): An integer barcode of the product.
         image (str): The picture of the product.
+
     """
+
     def __init__(self, product_name: str, product_number: int, barcode: int, image: str):
         self.name = product_name
         self.number = product_number
@@ -17,11 +20,13 @@ class Item:
 
 
 class Shelf:
-    """
+    """Shelf class
+
     Attrs:
         all_shelves (List[]): A list containing all the shelves.
         shelf_number (int): An integer of the number of the shelf.
         product_nums (List[int]): A list of unique item identifications numbers.
+
     """
     all_shelves = []
 
@@ -38,13 +43,19 @@ class Cart:
     Attrs:
         items_stored: An empty list.
     """
+
     def __init__(self):
         self.items_stored = []
 
     def assign_cart(self, shelf: object):
+        """Assign the cart object to a shelf object"""
         self.shelf = shelf
 
-    def scan_items_onto_cart(self, items: List):
+    def scan_onto_cart(self, items: List[object]):
+        """Scan items onto the cart if the cart is assigned to a shelf
+        Args:
+            items: a list of item objects
+        """
         if hasattr(self, "shelf"):
             items_added = []
             for item in items:
@@ -54,7 +65,8 @@ class Cart:
             for item in items_added:
                 items.remove(item)
 
-    def put_items_onto_shelf(self):
+    def scan_onto_shelf(self):
+        """Scan items into compartments in the shelf, from the trolly"""
         if hasattr(self, "shelf"):
             items_added = []
             for item in self.items_stored:
@@ -68,7 +80,6 @@ class Cart:
                 self.items_stored.remove(item)
 
 
-
 class Bin:
     """
     Attrs:
@@ -80,13 +91,19 @@ class Bin:
     all_bins = []
 
     def __init__(self, order_number: int, product_num_and_quantity: Dict[str, int]):
+        """Create a bin object
+
+        Args:
+            order_number: the order number
+            product_num_and_quantity: a dictionary with product numbers as key terms and the quantity of each product as the value
+        """
         self.order_num = order_number
-        # {product_num: quantity} takes in product number in the case of multiple products having the same name
         self.products_needed = product_num_and_quantity
         self.items_contained = []
         Bin.all_bins.append(self)
 
-    def scan_items_into_bin(self):
+    def scan_into_bin(self):
+        """Scans items into the bin"""
         for item_num in self.products_needed.keys():
             for shelf in Shelf.all_shelves:
                 for product, info in shelf.compartments.items():
@@ -129,6 +146,7 @@ class Package:
         Package.all_packages.append(self)
 
     def pack_items(self):
+        """Put items into the package object"""
         items_packaged = []
         for container in Bin.all_bins:
             if container.order_num == self.order_num:
@@ -140,6 +158,10 @@ class Package:
             container.items_contained.remove(item)
 
     def send_to_truck(self, address: str):
+        """Send package object to the conveyor belt to be taken to the truck
+        Args:
+            address: the address the package is being sent to
+        """
         if len(self.items) != 0:
             for truck in Truck.all_trucks:
                 if self.order_num in truck.orders.keys():
@@ -177,24 +199,24 @@ shelf1 = Shelf(1111, [1, 3])
 cart1 = Cart()
 
 # won't do anything since cart not assigned to a shelf
-cart1.scan_items_onto_cart(items)
+cart1.scan_onto_cart(items)
 # print(cart1.items_stored)
 # print(items)
 
 cart1.assign_cart(shelf1)
 
 # items can now be scanned onto cart
-cart1.scan_items_onto_cart(items)
+cart1.scan_onto_cart(items)
 # print(cart1.items_stored)
 # print(items)
 
-cart1.put_items_onto_shelf()
+cart1.scan_onto_shelf()
 # print(shelf1.compartments)
 # print(cart1.items_stored)
 
 bin1 = Bin(123, {1: 4, 3: 2})
 # print(bin1.products_needed)
-bin1.scan_items_into_bin()
+bin1.scan_into_bin()
 # print(bin1.products_needed)
 # print(bin1.items_contained)
 
