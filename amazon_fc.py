4e101944820df5747553a5c558882b881b54b480
Joseph <joseph.zhang21@ycdsbk12.ca>
2019-10-24 12:14:09 -0400

    added main file

--- amazon_fc.py
+++ amazon_fc.py
@@ -1,25 +1,4 @@
 from typing import List, Dict
-import arcade
-
-screen_width = 800
-screen_height = 600
-
-def on_draw(delta_time):
-    arcade.start_render()
-    pass
-
-
-def on_key_press(key, modifiers):
-    pass
-
-
-def on_key_release(key, modifiers):
-    pass
-
-
-def on_mouse_press(x, y, button, modifiers):
-    pass
-
 
 class Item:
     """Item class
@@ -161,7 +140,7 @@ class Package:
         items: An empty list.
         stamped: A boolean value.
     """
-    
+
     all_packages = []
 
     def __init__(self, order_num: int, package_type: str):
@@ -259,34 +238,3 @@ truck1 = Truck(12, [123, 456])
 package1.send_to_truck("8101 Leslie Street")
 # print(truck1.orders)
 # print(truck1.orders[123].address)
-
-def setup():
-    arcade.open_window(screen_width, screen_height, "My Arcade Game")
-    arcade.set_background_color(arcade.color.BLACK)
-    arcade.schedule(on_update, 1/60)
-
-    # Override arcade window methods
-    window = arcade.get_window()
-    window.on_draw = on_draw
-    window.on_key_press = on_key_press
-    window.on_key_release = on_key_release
-    window.on_mouse_press = on_mouse_press
-
-
-def main():
-    arcade.open_window(screen_width, screen_height, "Amazon FC")
-    arcade.set_background_color(arcade.color.BLACK)
-
-    # Call on_draw every 60th of a second
-    arcade.schedule(on_draw, 1/60)
-    window = arcade.get_window
-    window.on_draw = on_draw
-    window.on_key_press = on_key_press
-    window.on_key_release = on_key_release
-    window.on_mouse_press = on_mouse_press
-    
-
-    arcade.run()
-
-if __name__ == "__main__":
-    main()
