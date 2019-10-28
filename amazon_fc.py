88e28bad675125254dbed3c9d823ed80a44c7a92
Joseph <joseph.zhang21@ycdsbk12.ca>
2019-10-28 12:46:53 -0400

    update

--- amazon_fc.py
+++ amazon_fc.py
@@ -1,5 +1,6 @@
 from typing import List, Dict
 
+
 class Item:
     """Item class
 
@@ -11,11 +12,12 @@ class Item:
 
     """
 
-    def __init__(self, product_name: str, product_number: int, barcode: int, image: str):
+    def __init__(self, product_name: str, product_number: int, barcode: int, image: str, length: int, width: int, height: int):
         self.name = product_name
         self.number = product_number
         self.barcode = barcode
         self.image = image
+        self.dimensions = (length, width, height)
 
 
 class Shelf:
@@ -81,6 +83,15 @@ class Cart:
                 self.items_stored.remove(item)
 
 
+class Order():
+    all_orders = {}
+
+    def __init__(self, number: int, product_num_and_quantity: Dict[int, int]):
+        self.number = number
+        self.products_needed = product_num_and_quantity
+        Order.all_orders[self.number]: "Incomplete"
+
+
 class Bin:
     """Bin class
 
@@ -93,41 +104,42 @@ class Bin:
 
     all_bins = []
 
-    def __init__(self, order_number: int, product_num_and_quantity: Dict[str, int]):
+    def __init__(self):
         """Create a bin object
 
         Args:
             order_number: the order number
             product_num_and_quantity: a dictionary with product numbers as key terms and the quantity of each product as the value
         """
-        self.order_num = order_number
-        self.products_needed = product_num_and_quantity
         self.items_contained = []
         Bin.all_bins.append(self)
 
+    def assign_order(self, order: object):
+        self.order = order
+
     def scan_into_bin(self):
         """Scans items into the bin"""
-        for item_num in self.products_needed.keys():
+        for item_num in self.order.products_needed.keys():
             for shelf in Shelf.all_shelves:
                 for product, info in shelf.compartments.items():
                     if item_num == product:
-                        while info["Quantity"] > 0 and self.products_needed[item_num] > 0:
+                        while info["Quantity"] > 0 and self.order.products_needed[item_num] > 0:
                             self.items_contained.append(
                                 info["Items Stored"][0])
                             info["Items Stored"].pop(0)
-                            self.products_needed[item_num] -= 1
+                            self.order.products_needed[item_num] -= 1
                             info["Quantity"] -= 1
-                        if self.products_needed[item_num] == 0:
-                            self.products_needed[item_num] = None
+                        if self.order.products_needed[item_num] == 0:
+                            self.order.products_needed[item_num] = None
                         else:
                             # reserve items that are currently available but order will not be sent out for shipping until order fulfilled for the item
-                            items_missing = self.products_needed[item_num]
+                            items_missing = self.order.products_needed[item_num]
                             print(
                                 f"Order Fulfillment Invalid: {items_missing} more items of (product number: {product}) needed to complete order.")
 
         # remove products from order that are scanned into bin
         self.products_needed = {item_num: quantity for item_num,
-                                quantity in self.products_needed.items() if quantity is not None}
+                                quantity in self.order.products_needed.items() if quantity is not None}
 
 
 class Package:
@@ -143,8 +155,9 @@ class Package:
 
     all_packages = []
 
-    def __init__(self, order_num: int, package_type: str):
-        self.order_num = order_num
+    def __init__(self, Bin: object, package_type: str):
+        self.order_num = Bin.order.number
+        self.bin = Bin
         self.type = package_type
         self.items = []
         self.stamped = False
@@ -153,14 +166,12 @@ class Package:
     def pack_items(self):
         """Put items into the package object"""
         items_packaged = []
-        for container in Bin.all_bins:
-            if container.order_num == self.order_num:
-                for item in container.items_contained:
-                    if item.number not in container.products_needed.keys():
-                        self.items.append(item)
-                        items_packaged.append(item)
+        for item in self.bin.items_contained:
+            if item.number not in self.bin.products_needed.keys():
+                self.items.append(item)
+                items_packaged.append(item)
         for item in items_packaged:
-            container.items_contained.remove(item)
+            self.bin.items_contained.remove(item)
 
     def send_to_truck(self, address: str):
         """Send package object to the conveyor belt to be taken to the truck
