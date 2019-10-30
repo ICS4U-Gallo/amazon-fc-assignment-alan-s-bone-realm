fd9fa71cf5c338354990b36912452e5a7ea5e406
josh <joshua.chuang21@ycdsbk12.ca>
2019-10-29 20:44:25 -0400

    update

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ import arcade
 screen_width = 800
 screen_height = 600
 iphone = afc.Item("iphone", 100, 200, "iphone.png", 10, 4, 1)
+steak = afc.Item("Steak", 2, 2049555, "steak.png", 100, 50, 5)
+fish = afc.Item("Fish", 500000032010340124, 12415, "fish.png", 20, 10, 54)
+shoe = afc.Item("Shoe", 1342135, 1251572457, "shoe.png", 30, 5, 10)
+
+shipment = [iphone, steak, fish, shoe]
+
 
 current_screen = 0
 
 clicked_prev = False
 clicked_next = False
-truck = False
+clicked_truck = False
+clicked_cart = False
 item = 0
 
 
@@ -37,6 +44,11 @@ class drawings:
                          400, 225, arcade.color.WHITE, 10, 200, "center", 'Arial',
                          True, False, "center", "center")
 
+    def cart(self):
+        arcade.draw_rectangle_outline(675, 50, 50, 50, arcade.color.WHITE)
+        arcade.draw_text("CART", 675, 50, arcade.color.WHITE, 9,
+                         200, "center", 'Arial', True, False, "center", "center")
+
     def truck(self):
         arcade.draw_rectangle_outline(750, 50, 50, 50, arcade.color.WHITE)
         arcade.draw_text("TRUCK", 750, 50, arcade.color.WHITE, 9, 
@@ -44,23 +56,37 @@ class drawings:
 
 
 def on_update(delta_time):
-    global clicked_prev, clicked_next, truck
+    global clicked_prev, clicked_next, clicked_truck, clicked_cart, shipment
     global item
+    shelf = 0
     if clicked_prev:
         if item > 0:
             item -= 1
+            shipment[item]
         print(item)
+        print(shipment[item])
         clicked_prev = False
 
     if clicked_next:
-        item += 1
+        if item < len(shipment) - 1:
+            item += 1
+            shipment[item]
         print(item)
+        print(shipment[item])
         clicked_next = False
-    
-    if truck:
+
+    if clicked_truck:
         print("truck")
-        truck = False
+        clicked_truck = False
         # New cart and trollys and bins add in when tests are done
+    
+    if clicked_cart:
+        print("cart")
+        afc.Cart.assign_cart(shipment, shelf)
+        for i in shipment:
+            afc.Cart.scan_onto_cart(shipment, shipment[i])
+        shelf += 1
+        clicked_cart = False
 
 
 def on_draw():
@@ -69,18 +95,11 @@ def on_draw():
     drawings.prev(iphone)
     drawings.next(iphone)
     drawings.truck(iphone)
+    drawings.cart(iphone)
 
 
 def on_key_press(key, modifiers):
-    global clicked_prev, clicked_next, truck
-    if key == arcade.key.A:
-        clicked_prev = True
-
-    if key == arcade.key.D:
-        clicked_next = True
-    
-    if key == arcade.key.T:
-        truck = True
+    pass
 
 
 def on_key_release(key, modifiers):
@@ -88,14 +107,23 @@ def on_key_release(key, modifiers):
 
 
 def on_mouse_press(x, y, button, modifiers):
-    global clicked_prev, clicked_next, truck
+    global clicked_prev, clicked_next, clicked_truck, clicked_cart
+    
+    # Previous button
     if (25 <= x <= 75) and (275 <= y <= 325):
         clicked_prev = True
+    
+    # Next button
     if (725 <= x <= 775) and (275 <= y <= 325):
         clicked_next = True
     
+    # Truck button
     if (725 <= x <= 775) and (25 <= y <= 75):
-        truck = True
+        clicked_truck = True
+    
+    # Cart button
+    if (650 <= x <= 700) and (25 <= y <= 75):
+        clicked_cart = True
 
 
 def main():
